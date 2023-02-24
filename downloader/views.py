d414b6777bd661bda4974f711e15215c5d50e8e1
Kaio <kaiosantos568@gmail.com>
2023-02-24 18:44:30 -0300

    Pequenas alterações e captura de erros

--- downloader/views.py
+++ downloader/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import FileResponse
 from django.contrib import messages
 from downloader.forms import DownloadForms
@@ -22,38 +22,50 @@ def download(request, cond):
 
         if form.is_valid():
             
-            if cond == "True":
-                print("youtube")
-            elif cond == "False":
-                print("spotify")
+            # cond == False: Spotify
+            # cond == True: Youtube
+            try:
+                if cond == "False":
+                    spotify = Spotify()
+
+                    if "track" in form["link"].value():
+                        spotify.track(form["link"].value())
+
+                    elif "playlist" in form["link"].value() and "youtube" not in form["link"].value():
+                        spotify.playlist(form["link"].value(), offset=int(form["offset"].value()))
+                    
+                    else:
+                        messages.error(request, "Link inválido")
+                        return redirect("index", {"form" : form, "classe" : CLASSE})
+
+                    spotify.download()
+                
+                elif cond == "True":
+                    youtube = Youtube()
+
+                    if "watch" in form["link"].value():
+                        youtube.track(form["link"].value())
+
+                    elif "playlist" in form["link"].value() and "spotify" not in form["link"].value():
+                        messages.info(request, "Essa funcionalidade ainda não existe.")
+
+                    else:
+                        messages.error(request, "Link inválido")
+                        return redirect("index.html", {"form" : form, "classe" : "youtube"})     
+
+                    youtube.download()
+                
+                with ZipFile('musics.zip', 'w') as zf:
+                    for file in os.listdir("musicas"):
+                        zf.write("musicas/" + file)
+
+                rmtree("musicas") # "limpando" a pasta de músicas
+
+                return FileResponse(open('musics.zip', 'rb'), as_attachment = True)
             
-            if cond == "False":
-                spotify = Spotify()
-
-                if "track" in form["link"].value():
-                    spotify.track(form["link"].value())
-
-                elif "playlist" in form["link"].value():
-                    spotify.playlist(form["link"].value(), offset=int(form["offset"].value()))
-
-                spotify.download()
-            
-            elif cond == "True":
-                youtube = Youtube()
-                if "watch" in form["link"].value():
-                    youtube.track(form["link"].value())
-                else:
-                    messages.error("erro")
-
-                youtube.download()
-            
-            with ZipFile('musics.zip', 'w') as zf:
-                for file in os.listdir("musicas"):
-                    zf.write("musicas/" + file)
-
-            rmtree("musicas") # "limpando" a pasta de músicas
-
-            return FileResponse(open('musics.zip', 'rb'), as_attachment = True)
+            except:
+                messages.error(request, "Erro no download")
+                return redirect("index", {"form" : form, "classe" : CLASSE})
     
     return render(request, "index.html", {"form" : form, "classe" : CLASSE})
 
